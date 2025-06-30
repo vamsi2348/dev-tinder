c0fe6afc576384d2080f910f07a5a3d1dd02dcb9
Vamsi <krishnasaivamsi143@gmail.com>
2025-06-30 16:37:44 +0530

    get,post,delete api calls

--- src/app.js
+++ src/app.js
@@ -8,21 +8,36 @@ const app = express();
 // })
 
 // So we need to configure routes which will return response based on the route
-app.use("/test",(req,res) => {
-    res.send("Hello from the server");
+// app.use("/test",(req,res) => {
+//     res.send("Hello from the server");
+// })
+
+// app.use("/",(req,res) => {
+//     res.send("Welcome to dev tinder");
+// })
+
+// // the order of routes matter if I given / at the start no other routes will be work
+// //app.use will map all the api calls
+// app.use((req, res) => {
+//   res.status(404).json({
+//     error: 'Route not found',
+//     path: req.originalUrl,
+//     method: req.method
+//   });
+// });
+
+//app.get
+app.get('/user',(req,res) => {
+    res.send({firstName:"raju","lastName": "yadav"})
 })
 
-app.use("/",(req,res) => {
-    res.send("Welcome to dev tinder");
+app.post('/user',(req,res) => {
+    res.send("Updated user in DB sucessfully")
 })
 
-app.use((req, res) => {
-  res.status(404).json({
-    error: 'Route not found',
-    path: req.originalUrl,
-    method: req.method
-  });
-});
+app.post('/delete',(req,res) => {
+    res.send("Deleted user sucessfully")
+})
 
 app.listen(3000,() => {
     console.log("Listening on port 3000");

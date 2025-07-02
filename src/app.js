89ec0ecd025391c79314351a8c70c4acbccebeeb
Vamsi <krishnasaivamsi143@gmail.com>
2025-07-02 21:07:09 +0530

    middle ware

--- src/app.js
+++ src/app.js
@@ -42,21 +42,54 @@ const app = express();
 
 
 //multi Route handlers
-app.use("/user",(req,res,next)=> {
-    //Route handler
-    console.log("Handler1");
+// app.use("/user",(req,res,next)=> {
+//     //Route handler
+//     console.log("Handler1");
+//     next();
+//     // res.send("First");
+// },(req,res) => {
+//     console.log("Handler2");
+//     res.send("Second");
+// },(req,res) => {
+//     console.log("Handler3");
+//     res.send("third");
+// },(req,res) => {
+//     console.log("Handler4");
+//     res.send("fouth");
+// })
+
+// 2nd way
+// app.use("/user",(req,res,next)=> {
+//     console.log("Handler1");
+//     next();
+// });
+// app.use("/user",(req,res,next)=> {
+//     console.log("Handler1");
+//     res.send("second")
+// });
+
+//third way
+// app.use('/user',[rh1,rh2,rh3.....]);
+
+//middle ware usage of next
+// here / is the main route handler which will check for all the routes so if it passes the required criteria then only it will pass to the next route?
+app.use('/',(req,res,next)=>{
+    console.log("Main handler");
     next();
-    // res.send("First");
-},(req,res) => {
-    console.log("Handler2");
-    res.send("Second");
-},(req,res) => {
-    console.log("Handler3");
-    res.send("third");
-},(req,res) => {
-    console.log("Handler4");
-    res.send("fouth");
-})
+});
+
+app.use('/user',(req,res,next)=>{
+    console.log("User handler");
+    res.send("User handled passed from main route");
+});
+
+
+app.use('/user',(req,res,next)=>{
+    console.log("User handler");
+    res.send("User handled passed from main route - 2nd");
+});
+
+
 
 app.listen(3000,() => {
     console.log("Listening on port 3000");
